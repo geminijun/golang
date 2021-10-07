310b32973f97ffefb43051935bba75b0b04e37e8
Jun Gan <geminijun@gmail.com>
2021-10-07 00:27:51 -0700

    Homework 1

--- httpserver/main.go
+++ httpserver/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"io"
 	"log"
 	"net/http"
@@ -15,18 +16,16 @@ import (
 func main() {
 	flag.Set("v", "4")
 	glog.V(2).Info("Starting http server...")
-	http.HandleFunc("/", rootHandler)
-	c, python, java := true, false, "no!"
-	fmt.Println(c, python, java)
-	err := http.ListenAndServe(":80", nil)
-	// mux := http.NewServeMux()
-	// mux.HandleFunc("/", rootHandler)
-	// mux.HandleFunc("/healthz", healthz)
+	// http.HandleFunc("/", rootHandler)
+	// err := http.ListenAndServe(":80", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("/healthz", healthz)
 	// mux.HandleFunc("/debug/pprof/", pprof.Index)
 	// mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
 	// mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 	// mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
-	// err := http.ListenAndServe(":80", mux)
+	err := http.ListenAndServe(":80", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,16 +33,32 @@ func main() {
 }
 
 func healthz(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("entering healthz handler")
+	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, "ok\n")
+}
+
+func GetIP(r *http.Request) string {
+	forwarded := r.Header.Get("X-FORWARDED-FOR")
+	if forwarded != "" {
+		return forwarded
+	}
+	return r.RemoteAddr
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("entering root handler")
-	user := r.URL.Query().Get("user")
-	if user != "" {
-		io.WriteString(w, fmt.Sprintf("hello [%s]\n", user))
+	ip := GetIP(r)
+	if ip != "" {
+		fmt.Println("IP: ", ip)
 	} else {
-		io.WriteString(w, "hello [stranger]\n")
+		fmt.Println("Can't find client's ip!\n")
+	}
+	version := os.Getenv("VERSION")
+	if version != "" {
+		io.WriteString(w, fmt.Sprintf("VERSION: [%s]\n", version))
+	} else {
+		io.WriteString(w, "Can't find version\n")
 	}
 	io.WriteString(w, "===================Details of the http request header:============\n")
 	for k, v := range r.Header {
